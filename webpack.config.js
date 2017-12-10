e3ec4c75ca5e1ba184467851380ce50bad0791aa
Dmitry Hays <haysdmitry@gmail.com>
2017-12-10 14:09:50 +0400

    Добавлен линтинг js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ const webpack = require("webpack");
 const config = {
     output: {
         filename: "script.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "eslint-loader",
+                options: {
+                    fix: true
+                }
+            }
+        ]
     }
 };
 
